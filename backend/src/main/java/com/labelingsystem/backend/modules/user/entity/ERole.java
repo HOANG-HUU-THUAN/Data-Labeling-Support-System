99e1417d34e3905ba6512f172d7ec156127510f9
sang <2251120047@ut.edu.vn>
2026-03-20 17:06:16 +0700

    [NCHDLSS-8] feat(auth): implement registration, login and logout

---
+++ backend/src/main/java/com/labelingsystem/backend/modules/user/entity/ERole.java
@@ -0,0 +1,8 @@
+package com.labelingsystem.backend.modules.user.entity;
+
+public enum ERole {
+    ADMIN,
+    MANAGER,
+    REVIEWER,
+    ANNOTATOR
+}
---
+++ backend/src/main/java/com/labelingsystem/backend/modules/user/entity/User.java
@@ -0,0 +1,58 @@
+package com.labelingsystem.backend.modules.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = "username"),
+            @UniqueConstraint(columnNames = "email")
+        })
+@Data
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, name ="password_hash")
+    private String password;
+
+    @Column(nullable = false)
+    private String status = "ACTIVE";
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
