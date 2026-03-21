63ae467b375f6c88a3d21dc37cad292993080730
Thanh Dat <datpt6404@ut.edu.vn>
2026-03-21 16:26:08 +0700

    feat: project list

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,7 +6,6 @@ import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
 import UsersPage from '../pages/UsersPage';
 import AuditLogsPage from '../pages/AuditLogsPage';
-import ProjectsPage from '../pages/ProjectsPage';
 import DatasetsPage from '../pages/DatasetsPage';
 import TasksPage from '../pages/TasksPage';
 import DashboardPage from '../pages/DashboardPage';
@@ -14,6 +13,9 @@ import ExportPage from '../pages/ExportPage';
 import MyTasksPage from '../pages/MyTasksPage';
 import AnnotationPage from '../pages/AnnotationPage';
 import ReviewPage from '../pages/ReviewPage';
+import ProjectList from '../pages/ProjectListPage';
+import ProjectCreate from '../pages/ProjectCreate';
+import ProjectDetail from '../pages/ProjectDetail';
 
 const withLayout = (element: React.ReactNode) => (
   <MainLayout>{element}</MainLayout>
@@ -33,7 +35,9 @@ const AppRoutes = () => {
       <Route path="/audit-logs" element={<ProtectedRoute roles={['ADMIN']}>{withLayout(<AuditLogsPage />)}</ProtectedRoute>} />
 
       {/* MANAGER only */}
-      <Route path="/projects" element={<ProtectedRoute roles={['MANAGER']}>{withLayout(<ProjectsPage />)}</ProtectedRoute>} />
+      <Route path="/projects" element={<ProtectedRoute roles={['MANAGER', 'ANNOTATOR']}>{withLayout(<ProjectList />)}</ProtectedRoute>} />
+      <Route path="/projects/create" element={<ProtectedRoute roles={['MANAGER', 'ANNOTATOR']}>{withLayout(<ProjectCreate />)}</ProtectedRoute>} />
+      <Route path="/projects/:id" element={<ProtectedRoute roles={['MANAGER', 'ANNOTATOR']}>{withLayout(<ProjectDetail />)}</ProtectedRoute>} />
       <Route path="/datasets" element={<ProtectedRoute roles={['MANAGER']}>{withLayout(<DatasetsPage />)}</ProtectedRoute>} />
       <Route path="/tasks" element={<ProtectedRoute roles={['MANAGER']}>{withLayout(<TasksPage />)}</ProtectedRoute>} />
       <Route path="/dashboard" element={<ProtectedRoute roles={['MANAGER']}>{withLayout(<DashboardPage />)}</ProtectedRoute>} />
