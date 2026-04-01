434f6c23194f45a65a65453506ff6b83cb2cd9cf
Hiếu Trần <tr.hieucyrus.93@gmail.com>
2026-04-01 16:15:47 +0700

    NCHDLSS-28: complete reviewer module UI (task list + workspace + bounding box + toggle + approve)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ReviewerTaskList from './features/reviewer/pages/ReviewerTaskList'
+import ReviewerWorkspace from './features/reviewer/pages/ReviewerWorkspace'
 import './App.css'
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/reviewer" element={<ReviewerTaskList />} />
+        <Route path="/reviewer/:taskId" element={<ReviewerWorkspace />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+
+/* Legacy template code (commented out)
+function LegacyApp() {
   const [count, setCount] = useState(0)
 
   return (
@@ -117,5 +130,5 @@ function App() {
     </>
   )
 }
+*/
 
-export default App
